3ca520f88bce8aba4dc75eaa4d3a582da4f08b4b
nadiakhristean <nadia.khristean@gmail.com>
2022-11-29 15:31:42 -0500

    Updates on validate

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -2,7 +2,6 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const axios = require('axios');
 
 const SALT_ROUNDS = 5;
 
@@ -10,10 +9,16 @@ const User = db.define('user', {
   firstName: {
     type: Sequelize.STRING,
     allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
   },
   lastName: {
     type: Sequelize.STRING,
     allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
   },
   email: {
     type: Sequelize.STRING,
