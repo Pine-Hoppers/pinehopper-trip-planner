7e668edf8f07515ba2a5389850696eeb8c5c03d6
nadiakhristean <nadia.khristean@gmail.com>
2022-11-30 18:58:53 -0500

    Added Signup Component, handled extra entry fields

--- client/Routes.js
+++ client/Routes.js
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { Login, Signup } from './components/AuthForm';
+import { Login } from './components/Login';
+import { Signup } from './components/Signup';
 import Home from './components/Home';
 import { me } from './store';
 
