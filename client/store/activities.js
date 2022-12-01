3f701bf7b7fd0344ec7153b5a173b4abbb3ede27
Christine Zheng <zchristine11@gmail.com>
2022-11-30 19:49:11 -0500

    created activities reducer

---
+++ client/store/activities.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import history from '../history';
+
+/**
+ * ACTION TYPES
+ */
+const SET_ACTIVITIES = 'SET_ACTIVITIES';
+
+/**
+ * ACTION CREATORS
+ */
+const setActivities = (activities) => ({ type: SET_ACTIVITIES, activities });
+
+/**
+ * THUNK CREATORS
+ */
+export const fetchParkActivities = (parkCode) => {
+  return async (dispatch) => {
+    try {
+      // make the GET request to NPS
+      const res = await axios.get(
+        `https://developer.nps.gov/api/v1/thingstodo?parkCode=${parkCode}&api_key=${process.env.API_KEY}`
+      );
+      dispatch(setActivities(res.data));
+      //   history.push('/login');
+    } catch (error) {
+      console.log('Unable to fetch park activities right now: ', error);
+      throw error;
+    }
+  };
+};
+
+/**
+ * REDUCER
+ */
+export default function (state = [], action) {
+  switch (action.type) {
+    case SET_ACTIVITIES:
+      return action.activities;
+    default:
+      return state;
+  }
+}
--- client/store/index.js
+++ client/store/index.js
@@ -1,14 +1,16 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import auth from './auth';
+import activities from './activities';
 
-const reducer = combineReducers({ auth })
+const reducer = combineReducers({ auth, activities });
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
-const store = createStore(reducer, middleware)
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+);
+const store = createStore(reducer, middleware);
 
-export default store
-export * from './auth'
+export default store;
+export * from './auth';
+export * from './activities';
