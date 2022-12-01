badfdd6ff1f898548a6c44f4f8095bafb968f504
nadiakhristean <nadia.khristean@gmail.com>
2022-11-30 19:10:25 -0500

    Added Signup/Login buttons on forms

---
+++ client/components/Signup.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { authenticate } from '../store';
+import { Link } from 'react-router-dom';
+
+/**
+ * COMPONENT
+ */
+const SignupForm = (props) => {
+  const { name, displayName, handleSubmit, error } = props;
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} name={name}>
+        <div>
+          <label htmlFor="firstName">
+            <small>First Name</small>
+          </label>
+          <input name="firstName" type="text" />
+        </div>
+        <div>
+          <label htmlFor="lastName">
+            <small>Last Name</small>
+          </label>
+          <input name="lastName" type="text" />
+        </div>
+        <div>
+          <label htmlFor="email">
+            <small>Email</small>
+          </label>
+          <input name="email" type="text" />
+        </div>
+        <div>
+          <label htmlFor="username">
+            <small>Username</small>
+          </label>
+          <input name="username" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button type="submit">{displayName}</button>
+        </div>
+        {error && error.response && <div> {error.response.data} </div>}
+      </form>
+      <h4>Already have an account?</h4>
+      <Link to="/login">
+        <button>Login Here</button>
+      </Link>
+    </div>
+  );
+};
+
+/**
+ * CONTAINER
+ *   Note that we have two different sets of 'mapStateToProps' functions -
+ *   one for Login, and one for Signup. However, they share the same 'mapDispatchToProps'
+ *   function, and share the same Component. This is a good example of how we
+ *   can stay DRY with interfaces that are very similar to each other!
+ */
+
+const mapSignup = (state) => {
+  return {
+    name: 'signup',
+    displayName: 'Sign Up',
+    error: state.auth.error,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    handleSubmit(evt) {
+      evt.preventDefault();
+      const formName = evt.target.name;
+      const username = evt.target.username.value;
+      const password = evt.target.password.value;
+      dispatch(authenticate(username, password, formName));
+    },
+  };
+};
+
+export const Signup = connect(mapSignup, mapDispatch)(SignupForm);
