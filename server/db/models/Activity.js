dcaa2f453a970de79b9c67ffbdd9765447ff3dc1
Christine Zheng <zchristine11@gmail.com>
2022-12-02 15:49:23 -0500

    update Activity model

--- server/db/models/Activity.js
+++ server/db/models/Activity.js
@@ -4,104 +4,51 @@ const db = require('../db');
 const Activity = db.define('activity', {
   activity_name: {
     type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
   url: {
     type: DataTypes.TEXT,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
   images: {
     type: DataTypes.ARRAY(DataTypes.TEXT),
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
   park_fullName: {
     type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
   park_url: {
     type: DataTypes.TEXT,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
   parkCode: {
     type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
   isReservationRequired: {
     type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
-
   feeDescription: {
     type: DataTypes.STRING,
   },
   doFeesApply: {
     type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
   arePetsPermittedWithRestrictions: {
     type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
   petsDescription: {
     type: DataTypes.TEXT,
   },
   activity_category: {
     type: DataTypes.ARRAY(DataTypes.STRING),
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
   activityDescription: {
     type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
   longDescription: {
     type: DataTypes.TEXT,
   },
   additional_category: {
     type: DataTypes.ARRAY(DataTypes.STRING),
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
   duration: {
     type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
   },
   accessibilityInformation: {
     type: DataTypes.TEXT,
