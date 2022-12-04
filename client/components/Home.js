26f78e546e5f4cedf0abb01b26fa5f7dd611bed3
nadiakhristean <nadia.khristean@gmail.com>
2022-12-03 19:10:00 -0500

    Added trips to home page and create trip button

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchTrips } from '../store/alltrips';
-
-// /**
-//  * COMPONENT
-//  */
-// export const Home = (props) => {
-//   const { username } = props;
-
-//   return (
-//     <div>
-//       <h3>Welcome, {username}</h3>
-//     </div>
-//   );
-// };
-
-// /**
-//  * CONTAINER
-//  */
-// const mapState = (state) => {
-//   return {
-//     username: state.auth.username,
-//   };
-// };
-
-// export default connect(mapState)(Home);
-
+import { Link } from 'react-router-dom';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import EditIcon from '@material-ui/icons/Edit';
 export class Home extends React.Component {
   constructor() {
     super();
@@ -43,7 +27,6 @@ export class Home extends React.Component {
 
   render() {
     const { trips } = this.props;
-    console.log('MY TRIPS', trips);
     const { username } = this.props;
 
     return (
@@ -52,8 +35,33 @@ export class Home extends React.Component {
           <h3>Welcome, {username}</h3>
         </div>
         <h4 id="trips">Upcoming Trips</h4>
-        {/* TODO: FIGURE OUT HOW TO GET TRIPS TO SHOW UP HERE, THEN STYLE THEM.
-        {trips} */}
+        <TableContainer component={Paper} className="trip-table">
+          <button type="submit" onClick={this.isClicked}>
+            Create New Trip
+          </button>
+        </TableContainer>
+        <TableBody>
+          {trips.map((trip) =>
+            this.state.editId === trip.id ? (
+              <TableRow
+                key={trip.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <EditTrip trip={trip} onClickUpdate={this.isEditClicked} />
+              </TableRow>
+            ) : (
+              <TableRow
+                key={trip.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  <Link to={`/my-planner/${trip.id}`}>{trip.tripName}</Link>
+                </TableCell>
+                <TableCell align="right"></TableCell>
+              </TableRow>
+            )
+          )}
+        </TableBody>
       </div>
     );
   }
