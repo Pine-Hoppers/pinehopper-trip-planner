9010332e313c2f81b5bb726333c12c785d9df223
Christine Zheng <zchristine11@gmail.com>
2022-12-06 18:00:34 -0500

    update Wishlist layout & link item to SingleActivity

--- client/components/Wishlist.js
+++ client/components/Wishlist.js
@@ -1,47 +1,56 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import CloseIcon from '@material-ui/icons/Close';
 
 export class Wishlist extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     const { wishlist } = this.props;
 
     return (
-      <div>
+      <main>
         <h1> Wishlist</h1>
-        {wishlist.length === 0 ? (
-          <h3>
-            Your wishlist is empty now!
-            <a href="/explore">Explore</a> another park & Add activities into
-            your wishlist!
-          </h3>
-        ) : (
-          wishlist.map((item) => {
-            const image = JSON.parse(item.activity.images);
+        <div className="all-activities-layout">
+          {wishlist.length === 0 ? (
+            <p>
+              Your wishlist is empty now!
+              <a href="/explore">Explore</a> another park & Add activities into
+              your wishlist!
+            </p>
+          ) : (
+            wishlist.map((item) => {
+              const image = JSON.parse(item.activity.images);
 
-            return (
-              <div
-                className="each-activity-layout"
-                draggable="true"
-                key={item.id}
-                onDragStart={() =>
-                  this.props.handleDragStart({
-                    title: item.activity.activity_name,
-                    name: item.activity.activity_name,
-                  })
-                }
-              >
-                <img className="all-activities-img" src={image.url} />
-                <div className="each-activity-detail">
-                  <p>{item.activity.activity_name}</p>
+              return (
+                <div
+                  key={item.id}
+                  className="each-activity-layout"
+                  draggable="true"
+                  onDragStart={() =>
+                    this.props.handleDragStart({
+                      title: item.activity.activity_name,
+                      name: item.activity.activity_name,
+                    })
+                  }
+                >
+                  <Link
+                    to={`/explore/${item.activity.parkCode}/activities/${item.activity.activity_id}`}
+                  >
+                    <img className="all-activities-img" src={image.url} />
+                    <div className="each-activity-detail">
+                      <p>{item.activity.activity_name}</p>
+                    </div>
+                  </Link>
                 </div>
-              </div>
-            );
-          })
-        )}
-      </div>
+              );
+            })
+          )}
+        </div>
+      </main>
     );
   }
 }
