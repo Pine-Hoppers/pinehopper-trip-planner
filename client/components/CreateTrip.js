d199adb322b18cd405edc7dc3f40a99379b6f666
Lu Miao <miaolphoebe@gmail.com>
2022-12-09 22:35:48 -0500

    minor update

--- client/components/CreateTrip.js
+++ client/components/CreateTrip.js
@@ -7,7 +7,7 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
 import { createTrip, updateTrip } from '../store/alltrips';
-import { fetchWishlist, removeItemFromWishlist } from '../store/wishlist';
+import { fetchWishlist } from '../store/wishlist';
 import { fetchSingleTrip } from '../store/singleTrip';
 
 /**
